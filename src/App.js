a71558adce618fc59a8610c3654c203c63635ea6
Skronak <Gllme.couturier@gmail.com>
2025-05-15 14:21:07 +0200

    Update App.js
    howToPlay & minor fixes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import BoardInspector from './components/board/BoardInspector.js';
 import Board from './components/board/Board.js';
 import data from './data.json';
 import './app.css';
-import seedrandom from 'seedrandom';
 import detective from "./img/avatar-detective.png";
 import criminal from "./img/avatar-criminal.png";
 import innocent from "./img/avatar-innocent.png";
 import preview from "./img/preview.svg";
 import AnimatedBackground from "./components/animatedBackground/AnimatedBackground.js";
 import ModalWrapper from "./components/modal/ModalWrapper";
-import {Button, Tooltip} from "@mui/material";
 import PreviewModal from "./components/previewModal/PreviewModal";
+import Tooltips from "./components/tooltips/Tooltips.tsx";
+import {randGeneratorFrom} from "./utils/random.ts";
+import HowToPlayButton from "./components/modal/HowToPlayButton.tsx";
 
 export default function App() {
     const [isGameVisible, toggleGameVisible] = useState(false);
@@ -28,12 +29,6 @@ export default function App() {
     const [openPreview, setOpenPreview] = useState(false);
     const handleChange = (event) => {
         toggleIsDetective(event.target.checked);
-    };
-
-    const randGeneratorFrom = (seed) => {
-        const generator = seedrandom(seed);
-
-        return (min, max) => Math.floor(generator() * max) + min;
     };
 
     useEffect(() => {
@@ -54,7 +49,7 @@ export default function App() {
 
         if (+nbPlayers > (casedata.suspects.length)) {
             createRoles(casedata, lastSuspect);
-        };
+        }
 
         toggleScreen(e);
     }
@@ -103,18 +98,12 @@ export default function App() {
                     </>
                 ) : (
                     <form>
-                        <div className={'align-left'}>
-                        <input id='help-button' type="button" className="btn btn-success"
-                               onClick={(e) => setOpenModal(true)}
-                               value="COMMENT JOUER"/>
-                        </div>
+                            <HowToPlayButton/>
 
                         <div className={"bloc"}>
                             <div className={'label-with-tooltip'}>
-                                <div title={'Clé'}>Clé</div>
-                                <Tooltip title="Le coupable sera determiné à partir de cette clé. Elle doit  être commune à tous les joueurs. A changer si vous souhaiter rejouer la même affaire" enterTouchDelay={0}>
-                                        <Button className="tooltip-icon">?</Button>
-                                </Tooltip>
+                                <div title="seed">Seed</div>
+                                <Tooltips title="Ce champs doit être le même pour tous les joueurs. Sa valeur permet de déterminer le coupable et de rejouer les mêmes affaires "/>
                             </div>
                             <input type="text" className="form-control" onChange={e => setSeed(e.target.value)} min='0'
                                    value={seed}/>
@@ -122,9 +111,7 @@ export default function App() {
                         <div className={"bloc"}>
                             <div className={'label-with-tooltip'}>
                                 <div title={'nombre de joueurs'}>Nombre de joueurs</div>
-                                <Tooltip title="Nombre total de joueur incluant le détective" enterTouchDelay={0}>
-                                    <Button className="tooltip-icon">?</Button>
-                                </Tooltip>
+                                <Tooltips title="Nombre total de joueur incluant le détective"/>
                             </div>
                             <input type="number" className="form-control"
                                    onChange={e => setNbPlayers(e.target.value - 1)}
@@ -145,11 +132,9 @@ export default function App() {
                             <div id="collapse" className={isDetective ? "hide" : "show"}>
                                 <div className={'label-with-tooltip'}>
                                 <div>Vous êtes le témoin n°</div>
-                                    <Tooltip title={`Chaque témoin doit s'attribuer un numéro unique entre 1 et ${nbPlayers}`} enterTouchDelay={0}>
-                                        <Button className="tooltip-icon">?</Button>
-                                    </Tooltip>
+                                    <Tooltips title={`Chaque témoin doit s'attribuer un numéro unique entre 1 et ${nbPlayers}`}/>
                                 </div>
-                                <input type="tel" disabled={isDetective} className="form-control"
+                                <input type="number" disabled={isDetective} className="form-control"
                                        pattern="^-?[0-9]\d*\.?\d*$"
                                        onChange={e => setIdPlayer(+e.target.value > +nbPlayers ? nbPlayers : e.target.value)}
                                        min='1' value={+idPlayer}/>
@@ -160,7 +145,7 @@ export default function App() {
                         </div>
                         <div className={"bloc"}>
                             <div>
-                                <span className={'affaire-bloc-title'}>Affaire ({[...cases.keys()].length})</span>
+                                <span className="affaire-bloc-title">Affaire ({[...cases.keys()].length})</span>
                                 <img className={'button-icon'} alt='apercu de l"affaire' src={preview} onClick={()=>setOpenPreview(true)}/>
                             </div>
                             <select id='caseId' className="form-select" name="case" size={5} value={selectedCase}
@@ -180,14 +165,9 @@ export default function App() {
             {openModal? (
                 <ModalWrapper title={'Guide de jeu'} onClose={()=> setOpenModal(false)}>
 
-                    <div>
-                        <p>Principe : L'Inspecteur devra identifier les mots qui ressortent fréquemment dans les différents discours. Si un joueur n'a pas employé les mêmes mots que les autres, il s'agit peut-être du Coupable ! (car ce dernier n'a pas la même liste de mots que les autres joueurs).</p>
-                        <p>Lancement de la partie</p>
-                        <p>Tous les joueurs choisissent la même clé et la renseigne dans l'application. La clé permet de déterminer le coupable et de rejouer les mêmes enquêtes avec une clé différente</p>
-                    </div>
-                </ModalWrapper>): null}
+                </ModalWrapper>) : null}
             {openPreview ? (
-                <PreviewModal onClose={()=> setOpenPreview(false)} case={cases.get(selectedCase)}/>
+                <PreviewModal onClose={() => setOpenPreview(false)} case={cases.get(selectedCase)}/>
             ) : null}
         </div>
     )
